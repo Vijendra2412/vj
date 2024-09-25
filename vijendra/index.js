70550e013aa92eef1c415e1cfe14ed9c7a6ad6e5
Vijay <SharmaVijendra2412@gmail.com>
2024-09-25 13:09:10 +0530

    first commit

---
+++ vijendra/index.js
@@ -0,0 +1,54 @@
+// for(i=0;i<=5;i++){
+//     console.log(i )
+// }
+
+// var x =10
+// while(x>=8){
+//     console.log("hello vj")
+//     x++
+// }
+
+// function abc(){
+//     return "hey vijenrda!..."
+// }
+
+// var add=new Function("num1","num2","return num1+num2");
+// console.log(add(3,4))
+
+// var pow = new Function("num1","num2","return Math.pow(num1,num2)")
+// var pow=new Function("num1","num2","return Math.pow(num1,num2)"); 
+// console.log(pow(2,3))
+
+// var arr = [1,2,3,4]
+// var arr1 = [5,6,7,8]
+// console.log(Math.max.apply(null,arr))
+// console.log(Math.min.apply(null,arr))
+// arr.push.apply(arr,arr1)
+// console.log(arr)
+
+
+// let arr = [
+//     ["a",1],
+//     ["b",2]
+// ]
+// for(let i = 0;i<arr.length;i++){
+//     for(let  j=0;j<arr[i].length;j++){
+//         console.log(arr[i][j])
+//     }
+// }
+
+// let arr = [
+//     ["a", 1],
+//     ["b", 2]
+// ]
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+
+let arr = [1,2,3,4]
+arr.forEach(abc=>{
+    console.log(abc)
+})
